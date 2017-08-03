7583a91b6e8d29d1b0318767ff53879976d18491
Vaughn Antonyan <vaughnanton@gmail.com>
2017-08-02 20:03:35 -0700

    Rough code/comments

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,13 +1,41 @@
+class Hangman
+#when you initialize the this is what happens
+  def initialize
+    @word = ""
+    @guess = []
+    @false_letters = [""]
+    @win = false
+    @loss = false
+    @turns_remaining = 10
+    get_word
+    game_loop
+  end
+
 #option to open a saved game
+  def get_word
+    words = []
+    chosen_word = ""
 
-#at new game script should load dictionary
-#randomly select a word between 5-12 characters
+    File.open("dictionary.txt").each do |line|
+      words << line
+    end
 
-#show current guesses : A B C D
-#ask player to guess (case insensitive) or save game
+    chosen_word = words.rand
+    puts chosen_word
+  end
+
+  def game_loop
+  end
+
+
+end
+#method randomly select a word between 5-12 characters
+
+#method show current guesses : A B C D
+#method ask player to guess (case insensitive) or save game
   #if save game, save and exit
   #elsif guess continue below
-#say wehtehr right or wrong
-#show _ _ A _ _ A _ word
-#say how many guesses left
+#method say wehtehr right or wrong
+#method show _ _ A _ _ A _ word
+#method say how many guesses left
   #if out of guesses player loses
