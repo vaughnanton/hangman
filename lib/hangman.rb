aac1c5fe47993c8533e2da9b1c5afea9531f9202
Vaughn Antonyan <vaughnanton@gmail.com>
2017-08-02 18:01:53 -0700

    Code map ready

---
+++ lib/hangman.rb
@@ -0,0 +1,13 @@
+#option to open a saved game
+
+#at new game script should load dictionary
+#randomly select a word between 5-12 characters
+
+#show current guesses : A B C D
+#ask player to guess (case insensitive) or save game
+  #if save game, save and exit
+  #elsif guess continue below
+#say wehtehr right or wrong
+#show _ _ A _ _ A _ word
+#say how many guesses left
+  #if out of guesses player loses
